f5a3afb7b268b9984c44cc499c7fd7fa67c9af3d
Arpit Gogia <earpitg@gmail.com>
2016-01-09 00:40:55 +0530

    UI Changes

--- app/src/main/java/com/indiahacks16/fintech/qrmoney/activities/SignupActivity.java
+++ app/src/main/java/com/indiahacks16/fintech/qrmoney/activities/SignupActivity.java
@@ -3,38 +3,43 @@ package com.indiahacks16.fintech.qrmoney.activities;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.indiahacks16.fintech.qrmoney.Contents;
+import com.indiahacks16.fintech.qrmoney.QRCodeEncoder;
 import com.indiahacks16.fintech.qrmoney.R;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
-public class SignupActivity extends Activity {
+import java.io.File;
+import java.io.FileOutputStream;
 
+public class SignupActivity extends Activity {
     EditText mUserName;
     EditText mPassword;
     EditText mEmail;
     EditText mPhoneNumber;
     Button mSignUpButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-
         setContentView(R.layout.activity_signup);
-
         mUserName = (EditText) findViewById(R.id.name);
         mPassword = (EditText) findViewById(R.id.password);
         mEmail = (EditText) findViewById(R.id.email);
         mSignUpButton = (Button) findViewById(R.id.btnRegister);
         mPhoneNumber = (EditText) findViewById(R.id.phoneNumber);
-
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,27 +47,19 @@ public class SignupActivity extends Activity {
                 String password = mPassword.getText().toString();
                 String email = mEmail.getText().toString();
                 String phoneNumber = mPhoneNumber.getText().toString();
-
                 username = username.trim();
                 password = password.trim();
                 email = email.trim();
                 phoneNumber = phoneNumber.trim();
-
                 if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
-
                     AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
                     builder.setMessage(R.string.signup_error_message);
                     builder.setTitle(R.string.signup_error_title);
                     builder.setPositiveButton(android.R.string.ok, null);
-
                     AlertDialog dialog = builder.create();
                     dialog.show();
-
                 } else {
-                    //Success! Create the new User!
-
                     setProgressBarIndeterminateVisibility(true);
-
                     ParseUser newUser = new ParseUser();
                     newUser.setUsername(username);
                     newUser.setPassword(password);
@@ -70,37 +67,47 @@ public class SignupActivity extends Activity {
                     newUser.signUpInBackground(new SignUpCallback() {
                         @Override
                         public void done(ParseException e) {
-
                             setProgressBarIndeterminateVisibility(false);
-
                             if (e == null) {
-
-                                //Success
-
+                                generateQR(mPhoneNumber.getText().toString().trim());
                                 Intent i = new Intent(SignupActivity.this, MainActivity.class);
                                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                 startActivity(i);
                             } else {
-
                                 AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
                                 builder.setMessage(e.getMessage());
                                 builder.setTitle(R.string.signup_error_title);
                                 builder.setPositiveButton(android.R.string.ok, null);
-
                                 AlertDialog dialog = builder.create();
                                 dialog.show();
-
                             }
                         }
                     });
-
                 }
-
-
             }
         });
-
     }
-
+    void generateQR(String phoneNumber) {
+        QRCodeEncoder qrCodeEncoder =
+                new QRCodeEncoder(phoneNumber, null, Contents.Type.TEXT, BarcodeFormat.QR_CODE.toString(), 300);
+        try {
+            Bitmap bitmap = qrCodeEncoder.encodeAsBitmap();
+            String filePath = Environment.getExternalStorageDirectory()
+                    + "/qrmoney";
+            File dir = new File(filePath);
+            if(!dir.exists())
+                dir.mkdirs();
+            filePath += "/" + phoneNumber + ".png";
+            File file = new File(filePath);
+            FileOutputStream fOut = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+            SharedPreferences sp = getSharedPreferences("PHNO", MODE_PRIVATE);
+            sp.edit().putString("phno", phoneNumber).apply();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/indiahacks16/fintech/qrmoney/fragments/MyQRFragment.java
+++ app/src/main/java/com/indiahacks16/fintech/qrmoney/fragments/MyQRFragment.java
@@ -1,14 +1,29 @@
 package com.indiahacks16.fintech.qrmoney.fragments;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 import com.indiahacks16.fintech.qrmoney.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.List;
 
 public class MyQRFragment extends Fragment {
-
+    ImageView mImageQr;
+    ImageButton email, whatsapp, share;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +32,58 @@ public class MyQRFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_my_qr, container, false);
-
+        mImageQr = (ImageView) view.findViewById(R.id.image_qr);
+        SharedPreferences sp = getContext().getSharedPreferences("PHNO", Context.MODE_PRIVATE);
+        String name = sp.getString("phno", "") + ".png";
+        final String filePath = Environment.getExternalStorageDirectory()
+                + "/qrmoney/" + name;
+        Picasso.with(getContext()).load(new File(filePath)).into(mImageQr);
+        email = (ImageButton) view.findViewById(R.id.email);
+        whatsapp = (ImageButton) view.findViewById(R.id.whatsapp);
+        share = (ImageButton) view.findViewById(R.id.share);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mailClient = new Intent(Intent.ACTION_SEND);
+                Intent tempIntent = new Intent(Intent.ACTION_SEND);
+                tempIntent.setType("*/*");
+                List<ResolveInfo> resInfo = getActivity().getPackageManager()
+                        .queryIntentActivities(tempIntent, 0);
+                for (int i = 0; i < resInfo.size(); i++) {
+                    ResolveInfo ri = resInfo.get(i);
+                    if (ri.activityInfo.packageName.contains("android.gm")){
+                        mailClient.setComponent(new ComponentName(ri.activityInfo.packageName, ri.activityInfo.name));
+                        mailClient.setType("message/rfc822");
+                        mailClient.putExtra(Intent.EXTRA_SUBJECT, "My QR Money Code!!");
+                        mailClient.putExtra(Intent.EXTRA_TEXT, "Send money in a few seconds using the QR Money App");
+                        mailClient.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + filePath));
+                    }
+                }
+                startActivity(mailClient);
+            }
+        });
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "My QR Money Code");
+                sendIntent.setType("image/png");
+                sendIntent.setPackage("com.whatsapp");
+                sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + filePath));
+                startActivity(sendIntent);
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent share = new Intent(android.content.Intent.ACTION_SEND);
+                share.setType("image/*");
+                share.putExtra(Intent.EXTRA_STREAM, Uri.parse(filePath)); // Add image path
+                startActivity(Intent.createChooser(share, "Share image using"));
+            }
+        });
         return view;
     }
 }
